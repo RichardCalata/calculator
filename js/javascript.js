c7f1a5e01a069fa2f346a65e86dab7a1b706bdd7
richardcalata <calatar6@gmail.com>
2019-01-11 11:23:38 -0600

    javascript add variables

---
+++ js/javascript.js
@@ -0,0 +1,18 @@
+var btnOne = document.getElementById("calc-one");
+var btnTwo = document.getElementById("calc-two");
+var btnThree = document.getElementById("calc-three");
+var btnFour = document.getElementById("calc-four");
+var btnFive = document.getElementById("calc-five");
+var btnSix = document.getElementById("calc-six");
+var btnSeven = document.getElementById("calc-seven");
+var btnEight = document.getElementById("calc-eight");
+var btnNine = document.getElementById("calc-nine");
+var btnZero = document.getElementById("calc-zero");
+
+var btnDecimal = document.getElementById("calc-decimal");
+
+var btnAdd = document.getElementById("calc-add");
+var btnMinus = document.getElementById("calc-minus");
+var btnMultiply = document.getElementById("calc-multiply");
+var btnDivide = document.getElementById("calc-divide");
+var btnEquals = document.getElementById("calc-equals");
